aabcf34bdea2f4b014b088d6d5d0c773d801bbcd
abucquet <bucqueta@stanford.edu>
2021-06-03 14:25:58 -0700

    main changes

--- main.py
+++ main.py
@@ -5,19 +5,21 @@ This file contains the main code to run our draft simulautions
 import numpy as np
 from collections import defaultdict
 from typing import List, Dict
+import itertools as it
+from tqdm import tqdm
 
 from simulation import (
-	WNBA_TEAMS, simulateSeason
+	WNBA_TEAMS, simulateSeason, createGames
 	)
 from drafts import (
 	computeStandings, computeWeightedStandings, runClassicDraft, runBucketedDraft
 	)
 
-def main():
-	B = 1000
+def full_simulation_no_tanking():
+	B = 100
 
 	# vanilla draft
-	positionVSPreference = [[] for _ in range(len(WNBA_TEAMS))]
+	positionVSPreference = [[] for team in WNBA_TEAMS]
 
 	for _ in range(B):
 		teamRecords, gamesPlayed, teamWinLoss = simulateSeason()
@@ -28,13 +30,13 @@ def main():
 
 		# compiling results: keep track standings vs draft pick
 		for i, team in enumerate(standings):
-			positionVSPreference[i].append(draftResults[team])
+			positionVSPreference[team].append(draftResults[team])
 
 	print([np.mean(pos) for pos in positionVSPreference])
 
 	# bucketed draft
-	positionVSPreference = [[] for _ in range(len(WNBA_TEAMS))]
-
+	positionVSPreference = [[] for team in WNBA_TEAMS]
+	
 	for _ in range(B):
 		teamRecords, gamesPlayed, teamWinLoss = simulateSeason()
 
@@ -48,4 +50,43 @@ def main():
 
 	print([np.mean(pos) for pos in positionVSPreference])
 
-main()
+def full_simulation_with_tanking():
+	B = 100
+
+	# vanilla draft
+	positionVSPreference = {team: [] for team in WNBA_TEAMS for i in range(39)}
+
+	for tankingTeam in WNBA_TEAMS:
+		for tankingStart in range(0, 38 + 1):
+			for _ in range(B):
+				teamRecords, gamesPlayed, teamWinLoss = simulateSeason([tankingTeam], tankingStart)
+
+				standings = computeStandings(teamRecords)
+
+				draftResults = runClassicDraft(standings, noise=5)
+
+				# compiling results: keep track standings vs draft pick
+				for team in standings:
+					positionVSPreference[tankingTeam].append(draftResults[tankingTeam])
+
+	print({team: np.mean(pos) for team, pos in positionVSPreference.items()})
+
+	return
+
+	# bucketed draft
+	positionVSPreference = {team: [] for team in WNBA_TEAMS}
+	
+	for _ in range(B):
+		teamRecords, gamesPlayed, teamWinLoss = simulateSeason()
+
+		standings = computeStandings(teamRecords)
+
+		draftResults = runBucketedDraft(4, standings[::], teamRecords, noise=5)
+
+		# compiling results: keep track standings vs draft pick
+		for team in standings:
+			positionVSPreference[team].append(draftResults[team])
+
+	print({team: np.mean(pos) for team, pos in positionVSPreference.items()})
+
+full_simulation_no_tanking()
